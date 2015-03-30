931bf30e82fcbdccc1f5daa6789337e521323f81
Fred Romano <fmromano@vt.edu>
2015-03-30 17:01:38 -0400

    Simple DSA Cognitive engine with test scenario

--- cognitive_engines/CE_DSA.cpp
+++ cognitive_engines/CE_DSA.cpp
@@ -29,31 +29,42 @@ CE_DSA::~CE_DSA(){
 // execute function
 void CE_DSA::execute(void * _args){
 	struct CE_DSA_members * cm = (struct CE_DSA_members*) custom_members;
+    CognitiveRadio * CR = (CognitiveRadio *) _args;
 
-    // TODO: Check if packets received from other node are poor 
+    float current_rx_freq; 
+    // Check if packets received from other node are poor 
     // or not being received
+    if (!CR->CE_metrics.payload_valid)
+    {
+        // If so, switch to other rx frequency and tell
+        // other node to switch their tx frequency likewise.
+        current_rx_freq = CR->get_rx_freq();
+        if (current_rx_freq == cm->freq_a)
+        {
+            current_rx_freq = cm->freq_b;
+        }
+        else if (current_rx_freq == cm->freq_b)
+        {
+            current_rx_freq = cm->freq_a;
+        }
+        else if (current_rx_freq == cm->freq_x)
+        {
+            current_rx_freq = cm->freq_y;
+        }
+        else if (current_rx_freq == cm->freq_y)
+        {
+            current_rx_freq = cm->freq_x;
+        }
+    }
 
-    // TODO: If so, switch to other rx frequency and tell
-    // other node to switch their tx frequency likewise.
-    //if (current rx freq == freq_a)
-    //{
-    //    current_rx_freq = freq_b;
-    //}
-    //else if (current rx freq == freq_b)
-    //{
-    //    current_rx_freq = freq_a;
-    //}
-    //else if (current rx freq == freq_x)
-    //{
-    //    current_rx_freq = freq_y;
-    //}
-    //else if (current rx freq == freq_y)
-    //{
-    //    current_rx_freq = freq_x;
-    //}
+    // TODO: Put current rx freq in header of next packet
+    unsigned char * header = (unsigned char * ) &current_rx_freq;
+    CR->set_header(header);
     
     // TODO: Set tx freq to that specified by 
     // packet received from other node
+    float * new_tx_freq = (float *) CR->CE_metrics.header;
+    CR->set_tx_freq(*new_tx_freq);
 
 	//printf("Entered DSA's execute function\n");
 	//printf("The example metric is now %f\n", cm->example_ce_metric);
