4c9dd512fdb84dbaa3bc76043bae16ff1021113b
Jason Snyder <snyder84@vt.edu>
2015-08-24 14:58:01 -0400

    Cleaning up hopper CE

--- cognitive_engines/CE_Hopper.cpp
+++ cognitive_engines/CE_Hopper.cpp
@@ -4,10 +4,10 @@
 // custom member struct
 struct CE_Hopper_members{
 	unsigned int num_received;
-    static const float freq1 = 829e6;
-    static const float freq2 = 833e6;
-    static const float freq3 = 837e6;
-    static const float freq4 = 841e6;
+    static const float freq1 = 834e6;
+    static const float freq2 = 838e6;
+    static const float freq3 = 842e6;
+    static const float freq4 = 846e6;
 };
 
 // custom function declarations
@@ -36,55 +36,45 @@ void CE_Hopper::execute(void * _args){
     {
         if(ECR->CE_metrics.header[0] == 1)
         {
-            printf("hopping to 1\n");
             ECR->set_tx_freq(cm->freq1);
         }
         else if(ECR->CE_metrics.header[0] == 2)
         {
-            printf("hopping to 2\n");
             ECR->set_tx_freq(cm->freq2);
         }
         else if(ECR->CE_metrics.header[0] == 3)
         {
-            printf("hopping to 3\n");
             ECR->set_tx_freq(cm->freq3);
         }
         else if(ECR->CE_metrics.header[0] == 4)
         {
-            printf("hopping to 4\n");
             ECR->set_tx_freq(cm->freq4);
         }
     }
     else//Timeout or data packet received
     {
-        if(ECR->CE_metrics.CE_event == ce_timeout || 
-            (ECR->CE_metrics.CE_event == ce_phy_event &&
-                    (!ECR->CE_metrics.header_valid || !ECR->CE_metrics.payload_valid)))
+        if(ECR->CE_metrics.CE_event == ce_timeout) 
         {
             unsigned char header[8] = {0, 0, 0, 0, 0, 0, 0, 0};
             float current = ECR->get_rx_freq();
             if(current == cm->freq1)
             {
                 ECR->set_rx_freq(cm->freq2);
-                printf("rx on 2\n");
                 header[0] = 2;
             }
             else if(current == cm->freq2)
             {
                 ECR->set_rx_freq(cm->freq1);
-                printf("rx on 1\n");
                 header[0] = 1;
             }
             else if(current == cm->freq3)
             {
                 ECR->set_rx_freq(cm->freq4);
-                printf("rx on 4\n");
                 header[0] = 4;
             }
             else if(current == cm->freq4)
             {
                 ECR->set_rx_freq(cm->freq3);
-                printf("rx on 3\n");
                 header[0] = 3;
             }
             ECR->transmit_packet(header, NULL, 0);
@@ -92,7 +82,6 @@ void CE_Hopper::execute(void * _args){
         else if(ECR->CE_metrics.CE_frame == ce_frame_data && ECR->CE_metrics.payload_valid)
         {
             cm->num_received++;
-            printf("received packet %u on %f\n", cm->num_received, ECR->get_rx_freq());
         }
     }
 }
