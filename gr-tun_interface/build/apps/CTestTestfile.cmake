851c34592af9449f99bbef2b894aa729e413a140
Jason Snyder <snyder84@vt.edu>
2015-09-23 12:07:12 -0400

    Removing build folder

--- gr-tun_interface/build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/ericps1/src/gnuradio/gr-tun_interface/apps
-# Build directory: /users/ericps1/src/gnuradio/gr-tun_interface/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- gr-tun_interface/build/docs/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /users/ericps1/src/gnuradio/gr-tun_interface/docs
-# Build directory: /users/ericps1/src/gnuradio/gr-tun_interface/build/docs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
