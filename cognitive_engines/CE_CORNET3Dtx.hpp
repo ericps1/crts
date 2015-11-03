5ba53bce3f4e19c98db813517fb4353ea0dbd405
Jason Snyder <snyder84@vt.edu>
2015-11-03 10:56:03 -0500

    Updated to CE_CORNET3Dtx to make it work with latest master
        Added a header file for CE_CORNET3Dtx
        updated makefile and ECR.cpp to use CE_CORNET3Dtx

---
+++ cognitive_engines/CE_CORNET3Dtx.hpp
@@ -0,0 +1,16 @@
+#ifndef _CE_CORNET3DTX_
+#define _CE_CORNET3DTX_
+
+#include "CE.hpp"
+
+class CE_CORNET3Dtx : public Cognitive_Engine {
+    
+    private:
+        float example_ce_metric;
+    public:
+        CE_CORNET3Dtx();
+        ~CE_CORNET3Dtx();
+        virtual void execute(void * _args);
+};
+
+#endif
