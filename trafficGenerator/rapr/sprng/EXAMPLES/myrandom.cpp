871a1bbf366610541bacfd7889f154c4e88f1e90
Jason Snyder <snyder84@vt.edu>
2015-11-03 13:25:21 -0500

    Removing trafficGenerator for now

--- trafficGenerator/rapr/sprng/EXAMPLES/myrandom.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include <cstdlib>
-
-using namespace std;
-
-double myrandom_()              /* remove _ before C compilation */
-{
-  return (double) rand()/RAND_MAX;
-}
--- trafficGenerator/rapr/sprng/SRC/communicate.h
+++
@@ -1,3 +0,0 @@
-extern void get_proc_info_mpi(int *myid, int *nprocs);
-extern int make_new_seed_mpi(void);
-
