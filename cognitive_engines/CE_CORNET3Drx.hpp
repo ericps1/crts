496f98628e7b09b7a9799c710c74d920cf126aa6
Jason Snyder <snyder84@vt.edu>
2015-11-18 13:57:12 -0500

    Adding CORNET3D CEs and scenario

---
+++ cognitive_engines/CE_CORNET3Drx.hpp
@@ -0,0 +1,21 @@
+#ifndef _CE_CORNET3DRX_
+#define _CE_CORNET3DRX_
+
+#include "CE.hpp"
+#include "timer.h" 
+
+class CE_CORNET3Drx : public Cognitive_Engine {
+    
+    private:
+        int counter;
+        int good_packets, bad_packets;
+        float per;
+        timer feedback_timer;
+        timer window_timer;
+    public:
+        CE_CORNET3Drx();
+        ~CE_CORNET3Drx();
+        virtual void execute(void * _args);
+};
+
+#endif
