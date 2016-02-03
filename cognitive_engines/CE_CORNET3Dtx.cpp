735aa8d462fee0b00ac0b28f06256baf993d9458
Jason Snyder <snyder84@vt.edu>
2016-02-03 15:46:22 -0500

    Updating cornet cognitive engines to give them the right execute signature

--- cognitive_engines/CE_CORNET3Dtx.cpp
+++ cognitive_engines/CE_CORNET3Dtx.cpp
@@ -49,10 +49,7 @@ CE_CORNET3Dtx::~CE_CORNET3Dtx() {
 }
 
 // execute function
-void CE_CORNET3Dtx::execute(void * _args){
-    // type cast pointer to cognitive radio object
-    ExtensibleCognitiveRadio * ECR = (ExtensibleCognitiveRadio *) _args;
-
+void CE_CORNET3Dtx::execute(ExtensibleCognitiveRadio *ECR){
     int i = 0;
 
     // setup file descriptor to listen for data on TCP controller link.
@@ -66,7 +63,7 @@ void CE_CORNET3Dtx::execute(void * _args){
     // if data is available, read it in
     if(select(newsockfd+1, &fds, NULL, NULL, &timeout)){
         // read the first byte which designates the message type
-        i = recv(newsockfd, buffer, 7, 0);
+        i = recv(newsockfd, buffer, 9, 0);
     }
 
     if (i < 0) 
@@ -184,6 +181,22 @@ void CE_CORNET3Dtx::execute(void * _args){
             ECR->set_tx_fec1(LIQUID_FEC_CONV_V39); 
         if (buffer[5]=='1' && buffer[6] == '1')
             ECR->set_tx_fec1(LIQUID_FEC_CONV_V615); 
+
+	if (buffer[8] == '1')
+	    ECR->set_tx_rate(200e3);
+	if (buffer[8] == '2')
+	    ECR->set_tx_rate(400e3);
+	if (buffer[8] == '3')
+	    ECR->set_tx_rate(500e3);
+	if (buffer[8] == '4')
+	    ECR->set_tx_rate(700e3);
+	if (buffer[8] == '5')
+	    ECR->set_tx_rate(800e3);
+	if (buffer[8] == '6')
+	    ECR->set_tx_rate(900e3);
+	if (buffer[8] == '7')
+	    ECR->set_tx_rate(5000e3);
+
     }
 }
 
