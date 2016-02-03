7b0113e94ace51387d3fb3978fc1fb80b6a32230
Fred Romano <fmromano@vt.edu>
2016-02-03 15:44:07 -0500

    Fixed bug in commit ad7118b1a0
    The -f option for choosing the master scenario file
    was not being read.

--- src/CRTS_controller.cpp
+++ src/CRTS_controller.cpp
@@ -126,7 +126,7 @@ int main(int argc, char **argv) {
 
   // interpret command line options
   int d;
-  while ((d = getopt(argc, argv, "hma:")) != EOF) {
+  while ((d = getopt(argc, argv, "hf:ma:")) != EOF) {
     switch (d) {
     case 'h':
       help_CRTS_controller();
