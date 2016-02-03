fa133826b11c8fffea6079d06191737c75c5d457
Shubhajeet Chatterjee <shubh92@vt.edu>
2016-02-03 16:29:46 -0500

    added ability to change frequency of CORNET CE

--- cognitive_engines/CE_CORNET3Drx.cpp
+++ cognitive_engines/CE_CORNET3Drx.cpp
@@ -57,6 +57,49 @@ CE_CORNET3Drx::~CE_CORNET3Drx() {
 // execute function
 void CE_CORNET3Drx::execute(ExtensibleCognitiveRadio *ECR){
 
+int i = 0;
+
+    // setup file descriptor to listen for data on TCP controller link.
+    fd_set fds;
+    struct timeval timeout;
+    timeout.tv_sec = 0;
+    timeout.tv_usec = 100;
+    FD_ZERO(&fds);
+    FD_SET(newsockfd, &fds);
+
+    // if data is available, read it in
+    if(select(newsockfd+1, &fds, NULL, NULL, &timeout)){
+        // read the first byte which designates the message type
+        i = recv(newsockfd, buffer, 8, 0);
+    }
+
+    if (i < 0)
+    {
+        perror("recv");
+    }
+    else if(i > 0)
+    {
+        //If user closes CORNET3D, it sends a message starting with 9 to tell 
+        //the CE to shut down. Killing CRTS_controller (which runs on the same
+        //node as the transmitter) causes a graceful shutdown on both all nodes
+       if (buffer[7]=='1')
+       float rx_freq=140e6;
+       if (buffer[7]=='2')
+       float rx_freq=160e6;
+       if (buffer[7]=='3')
+       float rx_freq=500e6;
+       if (buffer[7]=='4')
+       float rx_freq=770e6;
+       if (buffer[7]=='5')
+       float rx_freq=1800e6;
+       if (buffer[7]=='6')
+       float rx_freq=1900e6;
+       if (buffer[7]=='7')
+       float rx_freq=3500e6;
+       ECR->set_rx_freq(tx_frq);
+    }
+
+
     if(ECR->CE_metrics.CE_event != ExtensibleCognitiveRadio::TIMEOUT)
     {
         if(ECR->CE_metrics.control_valid && ECR->CE_metrics.payload_valid)
